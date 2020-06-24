fcfe1c3edb37304ad4c45b0cb3e13a680ad28e72
Tim <mrtechguy234@gmail.com>
2020-06-24 17:13:47 -0400

    added tests for pdf

--- src/test/datascience/export/exportManagerFileOpener.unit.test.ts
+++ src/test/datascience/export/exportManagerFileOpener.unit.test.ts
@@ -29,6 +29,7 @@ suite('Data Science - Export File Opener', () => {
         applicationShell = mock<IApplicationShell>();
         browserService = mock<IBrowserService>();
         const reporter = mock(ProgressReporter);
+
         // tslint:disable-next-line: no-any
         when(reporter.createProgressIndicator(anything())).thenReturn(instance(mock<IDisposable>()) as any);
         when(documentManager.openTextDocument(anything())).thenResolve();
@@ -77,6 +78,34 @@ suite('Data Science - Export File Opener', () => {
         );
 
         await fileOpener.export(ExportFormat.html, model);
+
+        verify(browserService.launch(anything())).never();
+    });
+    test('Exporting to PDF displays message if operation fails', async () => {
+        when(exporter.export(anything(), anything())).thenThrow(new Error('Export failed...'));
+        when(applicationShell.showErrorMessage(anything())).thenResolve();
+        await fileOpener.export(ExportFormat.pdf, model);
+        verify(applicationShell.showErrorMessage(anything())).once();
+    });
+    test('PDF File opened if yes button pressed', async () => {
+        const uri = Uri.file('test.pdf');
+        when(exporter.export(anything(), anything())).thenResolve(uri);
+        when(applicationShell.showInformationMessage(anything(), anything(), anything())).thenReturn(
+            Promise.resolve(getLocString('DataScience.openExportFileYes', 'Yes'))
+        );
+
+        await fileOpener.export(ExportFormat.pdf, model);
+
+        verify(browserService.launch(anything())).once();
+    });
+    test('PDF File not opened if no button button pressed', async () => {
+        const uri = Uri.file('test.pdf');
+        when(exporter.export(anything(), anything())).thenResolve(uri);
+        when(applicationShell.showInformationMessage(anything(), anything(), anything())).thenReturn(
+            Promise.resolve(getLocString('DataScience.openExportFileNo', 'No'))
+        );
+
+        await fileOpener.export(ExportFormat.pdf, model);
 
         verify(browserService.launch(anything())).never();
     });
